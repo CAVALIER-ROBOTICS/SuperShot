46669eb941e3621711ed83f12eb6d2ad2399d627
waltja <140131856+waltja@users.noreply.github.com>
2024-02-22 10:09:22 -0600

    Update Main.java to reflect README

--- Main.java
+++ Main.java
@@ -5,12 +5,12 @@ public class Main {
     public static void main(String[] args) {
 
         double[] robotTestLocation = {1, 1, 0};
-        SuperShot heWasAShooterBoy = new SuperShot();
+        SuperShot superShot = new SuperShot();
 
-        heWasAShooterBoy.setShooterSpeed(12.0);
-        heWasAShooterBoy.update(robotTestLocation, false);
+        superShot.setShooterSpeed(12.0);
+        superShot.update(robotTestLocation, false);
         
-        System.out.println("Theta: " + heWasAShooterBoy.getTheta() + " Phi: " + heWasAShooterBoy.getPhi());
+        System.out.println("Theta: " + superShot.getTheta() + " Phi: " + superShot.getPhi());
         
     }
 
